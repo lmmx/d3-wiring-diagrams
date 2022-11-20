f082f7a739f143a96b121e1bb656903b199d2c36
Louis Maddox <louismmx@gmail.com>
2022-11-20 12:06:13 +0000

    Make global config object

--- wiring-diagram.js
+++ wiring-diagram.js
@@ -1,39 +1,56 @@
-height = 750;
-width = 1500;
-r = 100;
-wd_canvas_space = 50;
-wd_y_offset = 20;
-padding = 50;
-scaling_threshold = 0.1;
-label_y_offset = wd_canvas_space / 2 - wd_y_offset; // guessed based on performance
-dark_mode = false;
-fg_col = dark_mode ? "white" : "black";
-bg_col = dark_mode ? "black" : "white";
-fg_edge_col = dark_mode ? "#ACE4F5" : "#ADADAD"; // DARK: blue grey, LIGHT: red grey
-fg_text_col = dark_mode ? fg_col : fg_col;
-leaf_col = dark_mode ? "#48ff76" : "#0085fa"; // DARK: green, LIGHT: blue
-label_holder_opacity = "0.3";
+class Config {
+  height = 750;
+  width = 1500;
+  radius = 100;
+  canvas_space = 50;
+  y_offset = 20;
+  padding = 50;
+  scaling_threshold = 0.1;
+  label_holder_opacity = 0.3;
+
+  constructor(dark_mode = false) {
+    this.dark_mode = dark_mode;
+  }
+
+  get label_y_offset() {
+    // guessed based on performance
+    return this.canvas_space / 2 - this.y_offset;
+  }
+  get fg_col() {
+    return this.dark_mode ? "white" : "black";
+  }
+  get bg_col() {
+    return this.dark_mode ? "black" : "white";
+  }
+  get fg_edge_col() {
+    // DARK: blue grey, LIGHT: red grey
+    return this.dark_mode ? "#ACE4F5" : "#ADADAD";
+  }
+  get fg_text_col() {
+    return this.dark_mode ? this.fg_col : this.fg_col;
+  }
+  get leaf_col() {
+    // DARK: green, LIGHT: blue
+    return this.dark_mode ? "#48ff76" : "#0085fa";
+  }
+}
+
+var config = new Config();
 
 // var circle_data = [[width / 2, height / 2]];
 var circle_data = [];
 
-x = d3.scaleLinear().domain([0, width]).range([0, width]);
-y = d3.scaleLinear().domain([0, height]).range([0, height]);
+x = d3.scaleLinear().domain([0, config.width]).range([0, config.width]);
+y = d3.scaleLinear().domain([0, config.height]).range([0, config.height]);
 
 xAxis = d3.axisBottom(x);
-// .ticks(((width + 2) / (height + 2)) * 10)
-// .tickSize(height)
-// .tickPadding(8 - height);
 
 yAxis = d3.axisRight(y);
-// .ticks(10)
-// .tickSize(width)
-// .tickPadding(8 - width);
 
 const svg = d3
   .select("body")
   .append("svg")
-  .attr("viewBox", [0, 0, width, height]);
+  .attr("viewBox", [0, 0, config.width, config.height]);
 
 view_debug_opacity = "0.0";
 axis_debug_opacity = "0.0";
@@ -43,11 +60,13 @@ const view = svg
   .attr("class", "view")
   .attr("x", 0.5)
   .attr("y", 0.5)
-  .attr("width", width - 1)
-  .attr("height", height - 1)
+  .attr("width", config.width - 1)
+  .attr("height", config.height - 1)
   .attr(
     "style",
-    `opacity: ${dark_mode ? 1.0 - view_debug_opacity : view_debug_opacity}`
+    `opacity: ${
+      config.dark_mode ? 1.0 - view_debug_opacity : view_debug_opacity
+    }`
   );
 
 const gX = svg
@@ -63,14 +82,14 @@ const gY = svg
 const wiring_diagram = svg
   .append("g")
   .attr("class", "wiring_diagram")
-  .attr("transform", `translate(0,${wd_y_offset})`);
+  .attr("transform", `translate(0,${config.y_offset})`);
 
 const zoom = d3
   .zoom()
   .scaleExtent([1, 50])
   .translateExtent([
     [0, 0],
-    [width, height],
+    [config.width, config.height],
   ])
   .filter(filter)
   .on("zoom", zoomed);
@@ -79,7 +98,7 @@ function zoomed({ transform }) {
   view.attr("transform", transform);
   gX.call(xAxis.scale(transform.rescaleX(x)));
   gY.call(yAxis.scale(transform.rescaleY(y)));
-  transform.y += wd_y_offset;
+  transform.y += config.y_offset;
   wiring_diagram.attr("transform", transform);
 }
 
@@ -101,15 +120,15 @@ const circles = wiring_diagram
   .enter()
   .append("circle")
   .attr("class", "node")
-  .attr("r", 2 * r)
+  .attr("r", 2 * config.radius)
   .attr("transform", function (d) {
     return "translate(" + d + ")";
   });
 
 var pack = d3
   .pack()
-  .size([width, height - wd_canvas_space])
-  .padding(padding);
+  .size([config.width, config.height - config.canvas_space])
+  .padding(config.padding);
 
 function Pack(data) {
   const root = pack(
@@ -145,10 +164,10 @@ function Pack(data) {
       return d.value;
     })
     .attr("fill", function (d) {
-      return d.children ? bg_col : leaf_col;
+      return d.children ? config.bg_col : config.leaf_col;
     })
     .attr("opacity", 0.25)
-    .attr("stroke", fg_edge_col)
+    .attr("stroke", config.fg_edge_col)
     .attr("stroke-width", 2);
   var label_holder = node.append("g").attr("class", function (d) {
     var is_leaf = d3.select(this.parentNode).select("circle").classed("leaf");
@@ -161,7 +180,7 @@ function Pack(data) {
   label.attr("data-scale_factor", function (d) {
     text_bbox = this.getBBox(); // bounding client rect misbehaves
     factor = fit_label_to_diameter(d, text_bbox);
-    return factor > -scaling_threshold ? "1.0" : 1.0 - factor;
+    return factor > -config.scaling_threshold ? "1.0" : 1.0 - factor;
   });
   label
     .attr("style", function (d) {
@@ -169,7 +188,7 @@ function Pack(data) {
       // console.log(`Scale: ${scale}`);
       return `font-size: ${scale}rem`;
     })
-    .attr("fill", fg_text_col);
+    .attr("fill", config.fg_text_col);
   label.attr("data-width", function (d) {
     return this.getBBox().width; // same as getComputedTextLength
   });
@@ -195,9 +214,9 @@ function Pack(data) {
     .insert("rect", "text")
     .attr("fill", function (holder) {
       var is_par = d3.select(this.parentNode).classed("parental");
-      return is_par ? bg_col : "none";
+      return is_par ? config.bg_col : "none";
     })
-    .attr("fill-opacity", label_holder_opacity)
+    .attr("fill-opacity", String(config.label_holder_opacity))
     .attr("width", function (holder) {
       return d3.select(this.parentNode).select("text").attr("data-width");
     })
@@ -207,7 +226,7 @@ function Pack(data) {
     .attr("transform", function (holder) {
       label = d3.select(this.parentNode).select("text");
       [x_shift, y_shift] = label.attr("data-transform").split(",");
-      y_shift = parseFloat(y_shift) - wd_y_offset - label_y_offset;
+      y_shift = parseFloat(y_shift) - config.y_offset - config.label_y_offset;
       return `translate(${x_shift},${y_shift})`;
     });
 }
